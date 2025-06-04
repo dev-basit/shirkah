d87da4bedaff2890189ff412bc88d045210baf51
Abdul Basit <dev.basit2020@gmail.com>
2025-06-04 18:23:32 +0500

    added featured component

--- src/components/featured/featured.js
+++ src/components/featured/featured.js
@@ -1,5 +1,30 @@
 import { Navbar } from "@/components/navbar/navbar";
+import styles from "./featured.module.css";
+import Image from "next/image";
+import { ICONS } from "@/utils";
 
 export const Featured = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <main className={styles.featuredContainer}>
+        <div className={styles.featuredContentContainer}>
+          <div className={styles.featuredLeft}>
+            <p>We Specialize in the transportation</p>
+            <h1>Explore The Best Logistic & Transport</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Congue nunc molestie accumsan ipsum odio eu eget cras.
+              Massa sit risus quam magna et laoreet ullamcorper massa.
+            </p>
+            <button>Explore our Services</button>
+          </div>
+          <div className={styles.featuredRight}>
+            <div className={styles.featuredImageContainer}>
+              <Image src={ICONS.FEATURED} alt="featured" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
