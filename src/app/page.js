b0bc33e8b1dd6ef1fcbc9f736df8600000da853e
Abdul Basit <dev.basit2020@gmail.com>
2025-06-04 20:01:00 +0500

    added service container

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { Featured } from "@/components/featured/featured";
 import { Companies } from "@/components/companies/companies";
+import { Services } from "@/components/services/services";
 
 export default function Home() {
   return (
     <>
       <Featured />
       <Companies />
+      <Services />
     </>
   );
 }
---
+++ src/components/services/services.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import styles from "./services.module.css";
+import commonStyles from "../../app/common.module.css";
+import { ENUMS } from "@/utils";
+
+export const Services = () => {
+  return (
+    <div className={styles.servicesContainer}>
+      <div className={styles.headings}>
+        <p className={commonStyles.sectionName}>SERVICES</p>
+        <h1>
+          Our Quality <span>Services</span>
+        </h1>
+        <p className={commonStyles.text}>
+          We offer the best logistics and tracking for ambitious businesses.
+        </p>
+      </div>
+      <div className={styles.serviceTypesContainer}>
+        {ENUMS.SERVICES.map((service) => (
+          <div className={styles.serviceType} key={service.name}>
+            <div className={styles.serviceIcon}>
+              <Image
+                width={30}
+                height={30}
+                src={service.icon}
+                alt="service"
+                style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+              />
+            </div>
+            <p className={styles.serviceCategoryName}>{service.name}</p>
+            <p className={styles.serviceCategoryDescription}>
+              Lorem ipsum dolor sit amet consectetur. Ut sit integer maecenas eu.
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
