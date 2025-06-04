b1c684f14a8da8b62741da32e58858b7e543f04a
Abdul Basit <dev.basit2020@gmail.com>
2025-06-04 20:00:04 +0500

    added icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -2,10 +2,24 @@ import LOGO from "../../public/images/logo.svg";
 import FEATURED from "../../public/images/featured.svg";
 import FEATURED_BG from "../../public/images/featured-bg.svg";
 
+import TRUCK from "../../public/images/truck.svg";
+import ELECTRICITY from "../../public/images/electricity.svg";
+import OUTLINE from "../../public/images/outline.svg";
+import STORAGE from "../../public/images/storage.svg";
+import PACKING_BOX from "../../public/images/packing-box.svg";
+import CONTRACT from "../../public/images/contract.svg";
+
 const Icons = {
   LOGO,
   FEATURED,
   FEATURED_BG,
+
+  TRUCK,
+  ELECTRICITY,
+  OUTLINE,
+  STORAGE,
+  PACKING_BOX,
+  CONTRACT,
 };
 
 export default Icons;
