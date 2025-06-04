170972949857180499938a9dc6ec6854b876ba43
Abdul Basit <dev.basit2020@gmail.com>
2025-06-04 18:52:18 +0500

    added companies component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import { Featured } from "@/components/featured/featured";
+import { Companies } from "@/components/companies/companies";
 
 export default function Home() {
-  return <Featured />;
+  return (
+    <>
+      <Featured />
+      <Companies />
+    </>
+  );
 }
---
+++ src/components/companies/companies.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./companies.module.css";
+import { ENUMS, ICONS } from "@/utils";
+
+export const Companies = () => {
+  return (
+    <div className={styles.companiesContainer}>
+      <h2>
+        <pre>Trusted by more</pre>
+        <pre>
+          than <span> 4000</span> companies
+        </pre>
+      </h2>
+    </div>
+  );
+};
