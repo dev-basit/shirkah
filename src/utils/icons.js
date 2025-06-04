e2f2b768b8abeee9d7d312148d120d67fe18d020
Abdul Basit <dev.basit2020@gmail.com>
2025-06-04 18:18:57 +0500

    added icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,9 +1,11 @@
 import LOGO from "../../public/images/logo.svg";
 import FEATURED from "../../public/images/featured.svg";
+import FEATURED_BG from "../../public/images/featured-bg.svg";
 
 const Icons = {
   LOGO,
   FEATURED,
+  FEATURED_BG,
 };
 
 export default Icons;
