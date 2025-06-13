0d8aa410ec13fef593b0ccf85db288aa5136cdbd
Abdul Basit <dev.basit2020@gmail.com>
2025-06-13 18:22:18 +0500

    added service page icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,43 +1,55 @@
+import CIRCLE_MINUS from "../../public/images/minus-circle.svg";
+import CIRCLE_PLUS from "../../public/images/plus-circle.svg";
 import CONTAINER from "../../public/images/container.svg";
 import CONTAINER2 from "../../public/images/container-2.svg";
 import CONTAINER3 from "../../public/images/container-3.svg";
 import CONTAINER4 from "../../public/images/container-4.svg";
 import CONTRACT from "../../public/images/contract.svg";
-import CIRCLE_MINUS from "../../public/images/minus-circle.svg";
-import CIRCLE_PLUS from "../../public/images/plus-circle.svg";
+import CONTRACTING from "../../public/images/services/contracting.svg";
 import ELECTRICITY from "../../public/images/electricity.svg";
 import FEATURED from "../../public/images/featured.svg";
 import FEATURED_BG from "../../public/images/featured-bg.svg";
+import FULFILLMENT from "../../public/images/services/fulfillment.svg";
+import LM_SOLUTION from "../../public/images/services/lm-solution.svg";
 import LOGO from "../../public/images/logo.svg";
 import LOGO_WHITE from "../../public/images/logo-white.svg";
+import MANPOWER from "../../public/images/services/manpower.svg";
+import MAP from "../../public/images/map.svg";
 import OUTLINE from "../../public/images/outline.svg";
 import PACKING_BOX from "../../public/images/packing-box.svg";
 import STAR from "../../public/images/star.svg";
 import STORAGE from "../../public/images/storage.svg";
+import STORAGE2 from "../../public/images/services/storage.svg";
 import TICK_ORANGE from "../../public/images/tick-orange.svg";
+import TRANSPORT from "../../public/images/services/transport.svg";
 import TRUCK from "../../public/images/truck.svg";
-import MAP from "../../public/images/map.svg";
 
 const Icons = {
+  CIRCLE_MINUS,
+  CIRCLE_PLUS,
   CONTAINER,
   CONTAINER2,
   CONTAINER3,
   CONTAINER4,
   CONTRACT,
-  CIRCLE_MINUS,
-  CIRCLE_PLUS,
+  CONTRACTING,
   ELECTRICITY,
   FEATURED_BG,
   FEATURED,
-  LOGO,
+  FULFILLMENT,
+  LM_SOLUTION,
   LOGO_WHITE,
+  LOGO,
+  MANPOWER,
+  MAP,
   OUTLINE,
   PACKING_BOX,
   STAR,
   STORAGE,
+  STORAGE2,
   TICK_ORANGE,
+  TRANSPORT,
   TRUCK,
-  MAP,
 };
 
 export default Icons;
